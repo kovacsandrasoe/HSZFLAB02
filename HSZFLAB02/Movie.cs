80fcd45ca5b7bb202188f06c6022a3014d5d2186
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2026-09-16 10:00:43 +0200

    movie entity

---
+++ HSZFLAB02/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSZFLAB02
+{
+    public class Movie
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public int Duration { get; set; }
+
+        public int Year { get; set; }
+    }
+}
