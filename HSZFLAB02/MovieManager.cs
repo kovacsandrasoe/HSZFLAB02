095a3ef801ac9c366ba6d93083ea96027c74aee9
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2026-09-16 10:23:30 +0200

    MovieManager added

---
+++ HSZFLAB02/MovieManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSZFLAB02
+{
+    public class MovieManager
+    {
+        //publikusságot átírni később
+        List<Movie> Movies { get; set; } = new List<Movie>();
+
+        public void Add(Movie m)
+        {
+            Movies.Add(m);
+        }
+
+        public void Load()
+        {
+
+        }
+
+        public void Save()
+        {
+
+        }
+    }
+}
